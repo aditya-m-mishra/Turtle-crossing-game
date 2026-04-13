b7939639bc8e936bc0c34e19e42a38f1a6a894d9
aditya-m-mishra <adiapplies@gmail.com>
2026-04-13 13:31:57 +0530

    game over alert added

--- main.py
+++ main.py
@@ -27,10 +27,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # detect successful collision
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
         scoreboard.increase_level()
+
 screen.exitonclick()
